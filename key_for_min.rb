ee8663251e1765cd7355bfeb0fdee2e2d9758f26
Merline Belizaire <merline@noukod.com>
2018-09-27 18:31:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,14 @@
-birthday_kids.collect do |name, age|
-    age * 7
-end# prereqs: iterators, hashes, conditional logic
+# prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  hash_key = nil
+  hash_value = nil
+  hash.each do |a, b|
+    if hash_value == nil || b < hash_value
+      hash_value = b
+      hash_key = a
+    end
+  end
+  hash_key
 end
