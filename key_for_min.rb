371dc5d30ead72ed013afac2cbba2f1d2e5e9246
Merline Belizaire <merline@noukod.com>
2018-09-27 18:26:38 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,4 +1,6 @@
-# prereqs: iterators, hashes, conditional logic
+birthday_kids.collect do |name, age|
+    age * 7
+end# prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
